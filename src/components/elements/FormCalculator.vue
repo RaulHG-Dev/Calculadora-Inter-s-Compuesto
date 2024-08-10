3596c81985cfb0b48967ce44a654be8903d39e78
Raul Hidalgo <r.hidalgo.dev@gmail.com>
2024-08-09 19:29:29 -0600

    Update: Prinera parte de valdiación de formulario

--- src/components/elements/FormCalculator.vue
+++ src/components/elements/FormCalculator.vue
@@ -1,37 +1,84 @@
 <script setup>
+    import { FormKit } from '@formkit/vue';
     import Percent from '../icons/PercentIcon.vue';
-import Title from '../UI/Title.vue';
+    import Title from '../UI/Title.vue';
 </script>
 
 <template>
-    <form action="" class="font-poppins">
+    <div class="font-poppins">
         <Title class="h5">Ingrese información</Title>
-        <div class="mb-6">
-            <label for="default-input">Capital Inicial:</label>
-            <input type="number" id="default-input" class="input-default" min="1">
-        </div>
-        <div class="mb-6">
-            <label for="default-input">Interés:</label>
-            <div class="relative mb-6">
-                <div class="absolute inset-y-0 end-0 flex items-center pe-7 pointer-events-none">
-                    <Percent/>
+        <FormKit
+            type="form"
+            :classes="{
+                            input: 'w-full btn-green'
+                        }"
+        >
+            <div class="mb-4">
+                <FormKit
+                    type="number"
+                    class="input-default"
+                    label="Capital Inicial"
+                    name="capital-inicial"
+                    min="1"
+                    step="0.01"
+                    validation="required|number|min:1"
+                    :classes="{
+                        label: '!font-poppins',
+                        input: 'input-default !font-poppins',
+                        messages: 'text-xs text-red-400 !font-poppins',
+                    }"
+                />
+            </div>
+            <div class="mb-4 relative">
+                <FormKit
+                    type="number"
+                    class="input-default"
+                    label="Tasa de Interés (%)"
+                    name="interes"
+                    min="1"
+                    step="0.01"
+                    validation="required|number|min:1|max:100"
+                    :classes="{
+                        label: '!font-poppins',
+                        input: 'input-default !font-poppins',
+                        messages: 'text-xs text-red-400 !font-poppins'
+                    }"
+                />
+            </div>
+            <div class="mb-4 relative">
+                <FormKit
+                    type="number"
+                    class="input-default"
+                    label="Años"
+                    name="anios"
+                    min="1"
+                    step="1"
+                    validation="required|number|min:1|max:100"
+                    :classes="{
+                        label: '!font-poppins',
+                        input: 'input-default !font-poppins',
+                        messages: 'text-xs text-red-400 !font-poppins'
+                    }"
+                />
+            </div>
+            <div class="mb-6">
+                <div class="flex">
+                    <button type="button" class="w-full btn-gray dark:btn-blue">Limpiar</button>
+                    <FormKit
+                        type="submit"
+                        label="Calcular"
+                        :classes="{
+                            input: 'w-full btn-green'
+                        }"
+                    />
                 </div>
-                <input type="number" id="default-input" class="input-default" min="1" max="100">
             </div>
-        </div>
-        <div class="mb-6">
-            <label for="default-input">Años:</label>
-            <input type="number" id="default-input" class="input-default" min="1" max="100">
-        </div>
-        <div class="mb-6">
-            <div class="flex">
-                <button type="button" class="w-full btn-gray dark:btn-blue">Limpiar</button>
-                <button type="button" class="w-full btn-green">Calcular</button>
-            </div>
-        </div>
-    </form>
+        </FormKit>
+    </div>
 </template>
 
-<style lang="scss" scoped>
-
+<style>
+/* .formkit-icon {
+  max-width: 5em;
+} */
 </style>
